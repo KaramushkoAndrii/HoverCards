d5b44862a65336da069422f9a6c13c1954bf9c45
Andrey Karamushko <karamushko1997@gmail.com>
2024-02-12 12:09:49 +0200

    first version

---
+++ scripts/main.js
@@ -0,0 +1,66 @@
+'use strict'
+
+document.addEventListener('DOMContentLoaded', () => {
+    const cards = [{
+        name: 'beach',
+        url: './style/img/becah.jpg',
+        title: ' To the Beach!',
+        description: 'Description about beach'
+    },
+    {
+        name: 'space',
+        url: './style/img/space.jpg',
+        title: 'To the Space!',
+        description: 'Description about space'
+    },
+    {
+        name: 'desert',
+        url: './style/img/desert.jpg',
+        title: 'To the Desert!',
+        description: 'Description about desert'
+    },
+    {
+        name: 'mounts',
+        url: './style/img/mounts.jpg',
+        title: 'To the Mount!',
+        description: 'Description about mounth'
+    },
+    {
+        name: 'sea',
+        url: './style/img/sea.jpg',
+        title: 'To the sea!',
+        description: 'Descripton abput sea!'
+    }];
+    
+    const mainList = document.querySelector('.main__list');
+    
+    function createImageList(imageArr) {
+        imageArr.forEach(obj => {
+            const listItem = document.createElement('li');
+            
+            listItem.classList.add('main__item');
+            listItem.classList.add(`main__item--${obj.name}`);
+
+            listItem.style.backgroundImage = `url(${obj.url})`;
+    
+            const listHeading = document.createElement('h2');
+            listHeading.textContent = `${obj.title}`;
+    
+            const listDescription = document.createElement('p');
+            listDescription.textContent = `${obj.description}`;
+    
+            const btn = document.createElement('button');
+            btn.textContent = 'Click me!';
+    
+            listItem.appendChild(listHeading);
+            listItem.appendChild(listDescription);
+            listItem.appendChild(btn);
+    
+    
+            mainList.appendChild(listItem)
+        });
+    }
+    
+    createImageList(cards);
+})
+
